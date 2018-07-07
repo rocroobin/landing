9566362b73a8a172abf4e835206bd6282760ce56
rocroobin <sizpw@mail.ru>
2018-07-07 17:54:01 +0300

    небольшие изменения

--- snow.js
+++ snow.js
@@ -1,8 +1,11 @@
-let el = document.body.querySelectorAll('div');
-for (i = 0; i < 10; i += 1){
-let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
-let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
-// 50 - это размер картинки, чтобы не смещалась за пределы body
-el[i].style.left = x_position;
-el[i].style.top = y_position;
+let el = document.body.querySelectorAll('div.snowflake');
+let i = 0;
+let max = el.length;
+
+for (; i < max; i += 1){
+    let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
+    let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
+    // 50 - это размер картинки, чтобы не смещалась за пределы body
+    el[i].style.left = x_position;
+    el[i].style.top = y_position;
 }
