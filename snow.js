d9f7c1bb166589a5df6052952220edee86f0063e
rocroobin <sizpw@mail.ru>
2018-07-07 16:56:32 +0300

    рандомизация одной картинки

--- snow.js
+++ snow.js
@@ -1,3 +1,5 @@
 let el = document.getElementById('block1');
-el.style.left = 666;
-el.style.top = 666;
+let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
+let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
+el.style.left = x_position;
+el.style.top = y_position;
