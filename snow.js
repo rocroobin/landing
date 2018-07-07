e1f44001ac6dd37a17bae46f67d4df7cf9478f63
rocroobin <sizpw@mail.ru>
2018-07-07 17:42:51 +0300

    рандом для всех картинок

--- snow.js
+++ snow.js
@@ -1,5 +1,8 @@
-let el = document.getElementById('block1');
+let el = document.body.querySelectorAll('div');
+for (i = 0; i < 10; i += 1){
 let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
 let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
-el.style.left = x_position;
-el.style.top = y_position;
+// 50 - это размер картинки, чтобы не смещалась за пределы body
+el[i].style.left = x_position;
+el[i].style.top = y_position;
+}
