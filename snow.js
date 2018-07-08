dcc0bb1de5a5d141f89205d1e5fd6efa694c36ca
rocroobin <sizpw@mail.ru>
2018-07-08 14:16:08 +0300

    50 снежинок через js

--- snow.js
+++ snow.js
@@ -1,14 +1,17 @@
-let i = 0;
-let div = document.createElement('div');
-div.className = "snowflake";
-document.body.insertBefore(div, document.body.firstChild);
-let el = document.body.querySelectorAll('div.snowflake');
-let max = el.length;
+const snow = [];
+for (i = 0; i < 50; i += 1){
+    let div = document.createElement('div');
+    div.className = "snowflake";
+    document.body.insertBefore(div, document.body.firstChild);
+    snow[i] = div;
+}
+
+let max = snow.length;
     
-for (; i < max; i += 1){
+for (i = 0; i < max; i += 1){
     let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
     let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
     // 50 - это размер картинки, чтобы не смещалась за пределы body
-    el[i].style.left = x_position;
-    el[i].style.top = y_position;
+    snow[i].style.left = x_position;
+    snow[i].style.top = y_position;
 }
