7f69a6efbfb783f40c0875d76cee190aaef9677f
rocroobin <sizpw@mail.ru>
2018-07-08 12:06:49 +0300

    забыл убрать часть кода

--- snow.js
+++ snow.js
@@ -12,11 +12,3 @@ for (; i < max; i += 1){
     el[i].style.left = x_position;
     el[i].style.top = y_position;
 }
-
-for (; i < max; i += 1){
-    let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
-    let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
-    // 50 - это размер картинки, чтобы не смещалась за пределы body
-    div[i].style.left = x_position;
-    div[i].style.top = y_position;
-}
