b079d88fe276c6c145fa2c1f1a2c9435abbd6c0a
rocroobin <sizpw@mail.ru>
2018-07-08 12:03:59 +0300

    добавил снежинку через js и зарандомил ее

--- snow.js
+++ snow.js
@@ -1,11 +1,22 @@
-let el = document.body.querySelectorAll('div.snowflake');
 let i = 0;
+let div = document.createElement('div');
+div.className = "snowflake";
+document.body.insertBefore(div, document.body.firstChild);
+let el = document.body.querySelectorAll('div.snowflake');
 let max = el.length;
-
+    
 for (; i < max; i += 1){
     let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
     let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
     // 50 - это размер картинки, чтобы не смещалась за пределы body
     el[i].style.left = x_position;
     el[i].style.top = y_position;
+}
+
+for (; i < max; i += 1){
+    let x_position = Math.round(Math.random() * (document.body.offsetWidth - 50));
+    let y_position = Math.round(Math.random() * (document.body.offsetHeight - 50));
+    // 50 - это размер картинки, чтобы не смещалась за пределы body
+    div[i].style.left = x_position;
+    div[i].style.top = y_position;
 }
